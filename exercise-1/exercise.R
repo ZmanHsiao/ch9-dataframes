8eb050d542b4bce3daaf3a596d8f1c8fd849afb9
ZmanHsiao <ZmanHsiao@gmail.com>
2017-10-18 06:02:03 -0700

    first 3

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,20 +2,22 @@
 
 # Create a vector of the number of points the Seahawks scored in the first 4 games of their season
 # Hint: (google "Seahawks scores")
+gain <- c(48, 20, 26, 17)
 
 # Create a vector of the number of points the Seahawks have allowed to be scored against them in the first 4 games
-
+lose <- c(17, 13, 13, 13)
 
 # Combine your two vectors into a dataframe
-
+scores <- data.frame(gain, lose)
 
 # Create a new column "diff" that is the difference in points
-
+scores$diff <- gain - lose
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+scores$won <- scores$diff > 0
 
 # Create a vector of the opponents
-
+opponents <- c("Chargers", "Vikings", "Chiefs", "Raiders")
 
 # Assign your dataframe rownames of their opponents
+rownames(scores) <- opponents
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,43 +1,43 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employee <- paste(c("Employee"), 1:100)
 
 # Create a vector of 2014 salaries using the runif function
-
+salary.14 <- runif(100, min = 5, max = 20)
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+salary.15 <- runif(100, min = 8, max = 25)
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(employee, salary.14, salary.15)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- salary.15 - salary.14
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- salaries$raise > 0
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+salaries[57, "salary.15"]
 
 # How many employees got a raise?
-
+table(salaries$got.raise)
 
 # What was the value of the highest raise?
-
+which.max(salaries$raise)
 
 # What was the name of the employee who recieved the highest raise?
-
+salaries[which.max(salaries$raise), 1]
 
 # What was the largest decrease in salaries between the two years?
-
+min(salaries$raise)
 
 # What was the name of the employee who recieved largest decrease in salary?
-
+salaries[which.min(salaries$raise), 1]
 
 # What was the average salary increase?
-
+mean(salaries$raise)
 
 ### Bonus ###
 
